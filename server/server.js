26f456bee237c4c733cb51ea1e47c9181d6aa515
Diego Bolanos <blueashym@gmail.com>
2018-05-15 17:35:51 -0500

    fixed cors

--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+app.use(cors());
 
 app.use(require('./routes/encuesta'));
 
